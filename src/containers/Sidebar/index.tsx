2c57532fc20a2d4e45c7abf61a15371840b567cc
Felipe0l <felipeolivel@hotmail.com>
2024-08-16 13:02:35 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/felipe0l.png" />
+    <Titulo fontSize={20}>Felipe</Titulo>
+  </aside>
+)
+
+export default Sidebar
