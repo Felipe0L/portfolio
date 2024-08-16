645262c42a877fd5efd093942f3313c05490d6fa
Felipe0l <felipeolivel@hotmail.com>
2024-08-16 15:16:06 -0300

    finalizando implementação do tema

--- src/components/Projeto/index.tsx
+++ src/components/Projeto/index.tsx
@@ -1,13 +1,12 @@
 import Paragrafo from '../Paragrafo'
 import Titulo from '../Titulo'
 
-import { Card, LinkBotao } from './styles'
+import { Card } from './styles'
 
 const Projeto = () => (
   <Card>
     <Titulo>Projeto Lista de tarefas</Titulo>
     <Paragrafo tipo="secundario">Lista de tarefas feita com VueJS</Paragrafo>
-    <LinkBotao>Visualizar</LinkBotao>
   </Card>
 )
 
--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styled, { createGlobalStyle } from 'styled-components'
 
+import { Theme } from './themes/dark'
+
 const EstiloGlobal = createGlobalStyle`
   * {
   margin: 0;
@@ -13,6 +15,7 @@ const EstiloGlobal = createGlobalStyle`
   body {
     padding-top: 80px;
     padding-bottom: 80px;
+    background-color: ${(props) => (props.theme as Theme).corDeFundo};
 
     @media (max-width: 768px) {
       padding-top: 16px
@@ -35,9 +38,6 @@ export const Container = styled.div`
     display: block;
   }
 
-  img {
-    @media (max-width: 768px) {
-      width: 100%;
-    }
+
   }
 `
