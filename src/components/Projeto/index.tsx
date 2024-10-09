ddf8ddd8235028e73d2b00a057a83e4db0345fb1
Felipe0l <felipeolivel@hotmail.com>
2024-10-09 15:40:44 -0300

    implementado a interação com a api do github

--- src/components/Projeto/index.tsx
+++ src/components/Projeto/index.tsx
@@ -1,13 +1,24 @@
 import Paragrafo from '../Paragrafo'
 import Titulo from '../Titulo'
 
-import { Card } from './styles'
+import { Card, LinkBotao } from './styles'
 
-const Projeto = () => (
-  <Card>
-    <Titulo>Projeto Lista de tarefas</Titulo>
-    <Paragrafo tipo="secundario">Lista de tarefas feita com VueJS</Paragrafo>
-  </Card>
-)
+export type Props = {
+  title: string
+  url: string
+}
+
+const Projeto = ({ title, url }: Props) => {
+  return (
+    <Card>
+      <Titulo>{title}</Titulo>
+      <Paragrafo tipo="secundario">
+        <LinkBotao target="_blank" href={url}>
+          {url}
+        </LinkBotao>
+      </Paragrafo>
+    </Card>
+  )
+}
 
 export default Projeto
--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -1,17 +1,35 @@
+import { useEffect, useState } from 'react'
 import Projeto from '../../components/Projeto'
 import Titulo from '../../components/Titulo'
 
 import { Lista } from './styles'
 
-const Projetos = () => (
-  <section>
-    <Titulo fontSize={16}>Projetos</Titulo>
-    <Lista>
-      <li>
-        <Projeto />
-      </li>
-    </Lista>
-  </section>
-)
+type RepoProps = {
+  name: string
+  html_url: string
+}
+
+const Projetos = () => {
+  const [data, setData] = useState<RepoProps[]>([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/felipe0l/repos')
+      .then((res) => res.json())
+      .then((res) => setData(res))
+  }, [])
+
+  return (
+    <section>
+      <Titulo fontSize={16}>Projetos</Titulo>
+      <Lista>
+        {data.map((item) => (
+          <li key={item.name}>
+            <Projeto title={item.name} url={item.html_url} />
+          </li>
+        ))}
+      </Lista>
+    </section>
+  )
+}
 
 export default Projetos
